bf5e9d00cf7dde4dafc872b88e632afcf6041761
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-04-21 09:57:15 +0530

    #include<stdio.h> int main() {     int i,gcd,lcm,a,b;     scanf("%d%d",&a,&b);     for(i=1;i<=a && i<=b;i++)     {         if(a%i==0 && b%i==0)         {             gcd=i;         }     }     lcm=(a*b)/gcd;     printf("%d",lcm);     return 0; }

--- LCM.c
+++ LCM.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,gcd=0,lcm,i;
+    int i,gcd,lcm,a,b;
     scanf("%d%d",&a,&b);
-    for(i=1;i<=a&&i<=b;i++)
+    for(i=1;i<=a && i<=b;i++)
     {
-        if(a%i==0&&b%i==0)
+        if(a%i==0 && b%i==0)
         {
             gcd=i;
         }
