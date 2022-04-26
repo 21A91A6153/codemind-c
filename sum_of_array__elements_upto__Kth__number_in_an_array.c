41364cdbc9b4d5a87a92df62ac01ab0ce93e7145
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-04-26 10:36:47 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,sum=0,j,ind=0,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(arr[i]==k)         {             ind=i;             break;         }     }         for(j=0;j<=ind;j++)         {             sum=sum+arr[j];
            }
        printf("%d",sum);
    }

---
+++ sum_of_array__elements_upto__Kth__number_in_an_array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,sum=0,j,ind=0,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==k)
+        {
+            ind=i;
+            break;
+        }
+    }
+        for(j=0;j<=ind;j++)
+        {
+            sum=sum+arr[j];
+
+        }
+    printf("%d",sum);
+}
